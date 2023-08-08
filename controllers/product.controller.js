b07e0055f92604633addd8714c4e6a692931832e
Harry MacCode <emarh.harry.code@gmail.com>
2023-08-08 11:35:05 +0200

    Update a product

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,22 +1,78 @@
 const Product = require('../models/product.model');
 
-// Create a new Product => /api/v1/product/new
+// Create a new Product => /api/v1/products/new
 const newProduct = async (req, res, next) => {
-    const product = await Product.create(req.body);
+    try {
+        const product = await Product.create(req.body);
 
-    res.status(201).json({
-        success: true,
-        product
-    });
+        res.status(201).json({
+            success: true,
+            product
+        });
+    } catch(error) {
+        res.json(error.message);
+    }
 }
 
+// Get All Products => /api/v1/products
 const getProducts = async (req, res, next) => {
-    const products = await Product.find();
-    res.status(200).json({
-        success: true,
-        count: products.length,
-        products
-    });
+    try {
+        const products = await Product.find();
+        res.status(200).json({
+            success: true,
+            count: products.length,
+            products
+        });
+    } catch (error) {
+        res.json(error.message)
+    }
 }
 
-module.exports = { getProducts, newProduct };
+// Get Single Product details => /api/v1/products/:id
+const getSingleProduct = async (req, res, next) => {
+    try {
+        const product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: 'Produit non trouvé'
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            product
+        });
+    } catch (error) {
+        res.json(error.message);
+    }
+}
+
+const updateProduct = async (req, res, next) => {
+    try {
+        let product = await Product.findById(req.params.id);
+
+        if (!product) {
+            return res.status(404).json({
+                success: false,
+                message: 'Produit non trouvé'
+            });
+        }
+
+        product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true,
+            useFindAndModify: false
+        });
+
+        res.status(200).json({
+            success: true,
+            product
+        });
+    } catch (error) {
+        res.json(error.message);
+    }
+}
+
+module.exports = { getProducts, newProduct, getSingleProduct, updateProduct };
--- routes/product.route.js
+++ routes/product.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { getProducts, newProduct } = require('../controllers/product.controller');
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require('../controllers/product.controller');
 
 router.route('/products').get(getProducts);
 router.route('/products/new').post(newProduct);
+router.route('/products/:id').get(getSingleProduct);
+router.route('/products/:id').put(updateProduct);
 
 module.exports = router;
