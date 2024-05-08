974f66c6666a7a993f6285a6cadd4029e8b4bf47
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-08 23:52:26 +0200

    Register a User with Token generated

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,10 +5,12 @@ const register = asyncHanlder(async(req, res) => {
     const { name, email, password } = req.body;
 
     const user = await User.create({ name, email, password });
+    // const token = user.getJwtToken();
 
     res.status(201).json({
         success: true,
-        user
+        user,
+        // token
     });
 });
 
