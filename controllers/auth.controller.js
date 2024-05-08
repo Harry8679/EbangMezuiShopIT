feb7c3457e5cf187edab1fe395c54dcac5ff8f85
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-09 01:11:23 +0200

    Login User & Assign Token

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -16,7 +16,29 @@ const register = asyncHanlder(async(req, res) => {
 });
 
 const login = asyncHanlder(async(req, res, next) => {
-    res.send('Login');
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return next(new ErrorHandler('Please enter email and password', 400));
+    }
+
+    // Find the User in the database
+    const user = await User.findOne({ email }).select('password');
+
+    if (!user) {
+        return next('Invalid email or password', 401);
+    }
+
+    // Check if password is correct
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if (!isPasswordMatched) {
+        return next(new ErrorHandler('Invalid email or password', 401));
+    }
+
+    const token = user.getJwtToken();
+
+    res.status(200).json({ message: 'Login successful', token });
 });
 
 module.exports = { register, login };
--- models/user.model.js
+++ models/user.model.js
@@ -41,8 +41,13 @@ userSchema.pre('save', async function (next) {
 });
 
 // Return JWT Token
-userSchema.methods.getJwtToekn = function() {
+userSchema.methods.getJwtToken = function() {
     return jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES_TIME });
+}
+
+// Compare user password
+userSchema.methods.comparePassword = async function(enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
 }
 
 module.exports = mongoose.model('User', userSchema);
