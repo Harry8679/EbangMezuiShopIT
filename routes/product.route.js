f4f1143ad9a264e6a8a4dbd92672f435ca062906
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-09 14:50:55 +0200

    Protection for create, update and delete a product

--- routes/product.route.js
+++ routes/product.route.js
@@ -5,10 +5,10 @@ const router = express.Router();
 const { getProducts, newProduct, getSingleProduct, updateProduct, deleteProduct } = require('../controllers/product.controller');
 
 // router.route('/products').get(isAuthenticated, getProducts);
-router.route('/products').get(protected, authorizeRoles('admin'), getProducts);
-router.route('/admin/products').post(newProduct);
+router.route('/products').get(getProducts);
+router.route('/admin/products').post(protected, authorizeRoles('admin'), newProduct);
 router.route('/products/:id').get(getSingleProduct);
-router.route('/admin/products/:id').put(updateProduct);
-router.route('/admin/products/:id').delete(deleteProduct);
+router.route('/admin/products/:id').put(protected, authorizeRoles('admin'), updateProduct);
+router.route('/admin/products/:id').delete(protected, authorizeRoles('admin'), deleteProduct);
 
 module.exports = router;
